3c8398de6fd3ba300bed018f18171bbefecb4893
Gautham C Sudheer <csgautham091003@gmail.com>
2024-07-11 16:10:47 +0530

    Form styles

--- frontend/src/components/CreateEnquiryForm.js
+++ frontend/src/components/CreateEnquiryForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
-import "../styles/CreateEnquityForm.css";
+import "../styles/EnquiryForm.css";
 
 const CreateEnquiryForm = () => {
   const { user } = useAuthContext();
@@ -258,7 +258,7 @@ const CreateEnquiryForm = () => {
               onChange={(e) => handleDestinationChange(index, e.target.value)}
               className={emptyFields.includes("destinations") ? "error" : ""}
             />
-            <button
+            <button className="removeBtn"
               type="button"
               onClick={() => handleRemoveDestination(index)}
             >
@@ -266,7 +266,7 @@ const CreateEnquiryForm = () => {
             </button>
           </div>
         ))}
-        <button
+        <button className="addDestinationBtn"
           type="button"
           style={{ marginBottom: "20px" }}
           onClick={handleAddDestination}
@@ -415,8 +415,9 @@ const CreateEnquiryForm = () => {
           value={formData.remarks}
         />
       </div>
+      
+      <div className="submitBtn"><button type="submit">Add Enquiry</button></div>
 
-      <button type="submit">Add Enquiry</button>
       {error && <div className="error">{error}</div>}
     </form>
   );
