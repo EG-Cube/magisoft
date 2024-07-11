f7b03ccca8b553f029860e095e2d73819546f95c
Gautham C Sudheer <csgautham091003@gmail.com>
2024-07-11 12:32:22 +0530

    Login Style

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
+import { Link } from "react-router-dom";
+import "../styles/Login.css"
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -31,6 +33,9 @@ const Login = () => {
       />
       <button type="submit" disabled={isLoading}>Login</button>
       {error && <div className="error">{error}</div>}
+      <div className="signup-link">
+        Don't have an account? <Link to="/signup">Sign up</Link>
+      </div>
     </form>
   );
 };
