e68364ccb667b3b4c6c1035afdfa8b68a9531910
Gautham C Sudheer <csgautham091003@gmail.com>
2024-07-11 13:10:08 +0530

    Country Flag

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/TopNav.css";
 
 import leftImage from "../assets/magilogo.png";
-import rightImage from "../assets/england.png";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const TopNav = () => {
+  const { user } = useAuthContext();
+  const [countryFlag, setCountryFlag] = useState(null);
+
+  useEffect(() => {
+    const fetchCountryFlag = async () => {
+      if (user) {
+        try {
+          const flagModule = await import(`../assets/${user.user.country}.png`);
+          setCountryFlag(flagModule.default);
+        } catch (error) {
+          console.error(`Failed to load flag for ${user.user.country}`, error);
+          setCountryFlag(null);
+        }
+      }
+    };
+
+    fetchCountryFlag();
+  }, [user]);
+
   return (
     <div className="TopNav">
       <img src={leftImage} alt="Left Image" className="nav-image left" />
@@ -13,7 +32,7 @@ const TopNav = () => {
         <Link to="/enquiry/general" className="nav-button">
           New Enquiry
         </Link>
-        <img src={rightImage} alt="Right Image" className="nav-image right" />
+        {countryFlag && <img src={countryFlag} alt={user?.user.country} className="nav-image right" />}
       </div>
     </div>
   );
