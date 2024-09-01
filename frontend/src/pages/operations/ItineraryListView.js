3e5655fc4a3b4c11ea69350720a7a445f25a1344
ElviinThomasEldho <elviin.t.eldho@gmail.com>
2024-09-01 12:22:07 +0530

    minor test change

--- frontend/src/pages/operations/ItineraryListView.js
+++ frontend/src/pages/operations/ItineraryListView.js
@@ -25,6 +25,8 @@ const ItineraryListView = ({ type }) => {
       });
       const json = await response.json();
 
+      console.log(json);
+
       if (response.ok) {
         dispatch({ type: "SET_ITINERARIES", payload: json });
       }
